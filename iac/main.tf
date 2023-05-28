e188643de6f11f641f7f2efdaa47df27fade35c6
sankit chandel <sankit.sb.chandel@gmail.com>
2023-05-29 04:30:54 +0530

    updatd main.tf file to refer to external resource module

--- iac/main.tf
+++ iac/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "git::https://github.com/sb141/terraform.git//main/iac/modules/ResourceGroup"
+  source = "git::https://github.com/sb141/terraform.git//iac/modules/ResourceGroup"
   rg_name = var.resource_group_name
   location = var.location
 }
