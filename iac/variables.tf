9f6ecf31afe486eeefd551fdd3066f5a58741b35
sankit chandel <sankit.sb.chandel@gmail.com>
2023-05-29 03:49:30 +0530

    adding git workflow and terraform files

---
+++ iac/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default = "rg-test-skc-1245"
+  type = string
+}
+
+variable "location" {
+  description = "Azure region where the resources will be created"
+  default = "East US"
+  type = string
+}
