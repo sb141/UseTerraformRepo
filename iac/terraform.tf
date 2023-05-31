31190077acee8f999b2c8458fdbb17c11e9f12ce
sankit chandel <sankit.sb.chandel@gmail.com>
2023-05-31 13:41:11 +0530

    modified terraform.tf to include the correct config

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -13,6 +13,6 @@ terraform {
     resource_group_name = "rg-storage_state"
     storage_account_name = "staccstate123456"
     container_name = "tstate"
-    key = "var.az_storage_key"
+    key = "terraform.tfstate"
   }
 }
